65631f98afe6bfbdec95dfc1d6c2a6a6d3d35e58
Никита Царев <n.tsarev@worksolutions.ru>
2025-07-31 16:51:53 +0300

    16 chapter добавление метаданных и осознание для чего они нужны

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,13 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
-import {RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton} from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'dashboard',
+    description: 'Dashboard application',
+}
 
 export default async function Page() {
     return (
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata : Metadata = {
+    title: 'Customers',
+    description: 'Page for browse all customers? who use this application',
+}
+
 export default function Page() {
     return <p>Customers Page</p>;
 }
